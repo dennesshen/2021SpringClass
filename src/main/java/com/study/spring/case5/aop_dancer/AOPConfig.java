0d86b1674fa5558d787ac6c50af2436a0dff6507
denne <denne@DESKTOP-OIHJ20M>
2021-09-02 10:59:05 +0800

    Spring20210902Windows

--- src/main/java/com/study/spring/case5/aop_dancer/AOPConfig.java
+++ src/main/java/com/study/spring/case5/aop_dancer/AOPConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.w3c.dom.ls.LSOutput;
 
 //用java配置代替AOP-Config.xml
 //test1
@@ -14,7 +13,6 @@ import org.w3c.dom.ls.LSOutput;
 public class AOPConfig {
 	@Bean(name = "dancer")
 	public Performance dancer() {
-		System.out.println("testMac");
 		return new Dancer();
 	}
 	
